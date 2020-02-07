7da7b15e03ebcaf466a1acc04ce2b299e9cf8738
egor.vorobyev <egor@komus.net>
2020-02-07 19:23:42 +0300

    Всё решаю и решаю....

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public Solution(String a) {
+    }
+
+    protected Solution(int b) {
+    }
+
+    Solution(double d) {
+    }
+
+    private Solution() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+

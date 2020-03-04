50ce10ad70f0f3ed9b4c7a4b7d4134cc56c6931f
HDTestAccount <yas-1@yandex.ru>
2020-03-04 23:25:12 +0300

    всё решаю и решаю ....

--- 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Solution.java
@@ -1,12 +1,12 @@
 package com.javarush.task.task15.task1522;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /* 
 Закрепляем паттерн Singleton
 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) {
@@ -15,9 +15,29 @@ public class Solution {
 
     public static Planet thePlanet;
 
-    //add static block here - добавьте статический блок тут
+    static {
+        try {
+            readKeyFromConsoleAndInitPlanet();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static void readKeyFromConsoleAndInitPlanet() {
-        // implement step #5 here - реализуйте задание №5 тут
+    public static void readKeyFromConsoleAndInitPlanet() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = bufferedReader.readLine();
+        switch (input) {
+            case Planet.EARTH:
+                thePlanet = Earth.getInstance();
+                break;
+            case Planet.MOON:
+                thePlanet = Moon.getInstance();
+                break;
+            case Planet.SUN:
+                thePlanet = Sun.getInstance();
+                break;
+            default:
+                thePlanet = null;
+        }
     }
 }

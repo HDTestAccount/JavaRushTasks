552f2ffae3408b315c5535715dfda83e912342c9
egor <yas-1@yandex.ru>
2020-04-06 22:47:50 +0300

    Решаю )

--- 2.JavaCore/src/com/javarush/task/task17/task1712/Manager.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1712/Manager.java
@@ -7,10 +7,9 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Manager {      //singleton
     private static Manager ourInstance = new Manager();
-
-    private final List<Table> restaurantTables = new ArrayList<Table>(10);
     private int currentIndex = 0;
 
+    private final List<Table> restaurantTables = new ArrayList<Table>(10);
     private final Queue<Order> orderQueue = new ConcurrentLinkedQueue<Order>();        // очередь с заказами
     private final Queue<Dishes> dishesQueue = new ConcurrentLinkedQueue<Dishes>();     // очередь с готовыми блюдами
 
